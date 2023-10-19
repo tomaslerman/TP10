08858aa8a35b2bdf710cca45ce7e693c6d5166e6
tomaslerman <128831511+tomaslerman@users.noreply.github.com>
2023-10-19 14:02:41 -0300

    Add files via upload

---
+++ TP10/wwwroot/js/site.js
@@ -0,0 +1,62 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function MostrarTemporadas(IdS)
+{
+    $.ajax(     
+        {
+            type:'POST',
+            dataType:'JSON',
+            url:'/Home/infoTemporadas',
+            data:{IdSerie:IdS},
+            success:
+                function(response)
+                {
+                    $("#Info").html(response.numeroTemporada);
+                    $("#Info").html(response.tituloTemporada);
+                }
+
+
+        }
+
+    );
+}
+
+function MostrarActores(IdS)
+{
+    $.ajax(
+            {
+                type:'POST',
+                dataType:'JSON',
+                url:'/Home/infoActores',
+                data:{IdSerie:IdS},
+                success:
+                function(response)
+                {
+                    $("#Info").html(response.nombre);
+                }
+            }
+    );
+}
+
+function MostrarSerie(IdS)
+{
+    $.ajax(
+            {
+                type:'POST',
+                dataType:'JSON',
+                url:'/Home/infoSeries',
+                data:{IdSerie:IdS},
+                success:
+                function(response)
+                {
+                    $("#Info").html(response.nombre);
+                    $("#Info").html(response.añoInicio);
+                    $("#Info").html(response.sinopsis);
+                    $("#Info").html(response.imagenSerie);
+                }
+            }
+    );
+}
